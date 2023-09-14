52e23d4444ba1365e76f320187749af3050f77ac
Jasper Delight <jasperdelightt@gmail.com>
2023-09-14 15:52:58 -0600

    adds global prefix

--- keepr/Startup.cs
+++ keepr/Startup.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -92,6 +93,12 @@ public class Startup
       app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Jot v1"));
       app.UseCors("CorsDevPolicy");
     }
+    else
+    {
+      app.UseMiddleware<GlobalRoutePrefix>("/keepr");
+      app.UsePathBase(new PathString("/keepr"));
+    }
+
 
     app.UseHttpsRedirection();
 
