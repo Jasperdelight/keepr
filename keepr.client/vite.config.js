da97a75b4f495422f352d0a8d59b520b817599da
Jasper Delight <jasperdelightt@gmail.com>
2023-09-14 17:07:01 -0600

    fix path

--- keepr.client/vite.config.js
+++ keepr.client/vite.config.js
@@ -5,7 +5,7 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   plugins: [vue()],
   build: {
-    outDir: '../keepr/wwwwroot',
+    outDir: '../keepr/wwwroot',
     sourcemap: false
   },
   server: {
